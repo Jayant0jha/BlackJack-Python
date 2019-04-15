fff9c8910a5aa6545151e8eaabd33ae9a92dc73d
Jayant Ojha <31176672+Jayant0jha@users.noreply.github.com>
2019-04-15 19:33:02 +0530

    Dealer Class created with methods

--- blackjack.py
+++ blackjack.py
@@ -1,3 +1,8 @@
+import random
+
+Cards = ( "Ace", "2", "3", "4", "5", "6", "7","8", "9", "10", "Jack", "Queen", "King" )
+
+
 class Player(object):
 
     #Class Object Attribute
@@ -36,3 +41,47 @@ class Player(object):
                 value2 = int(card)
                 Player.sump += value2
         return Player.sump
+
+    
+class Dealer(Player):
+
+    #Class Object Attribute
+
+    dl = []
+    sumd = 0
+
+    def __init__(self):
+        Player.__init__(self)
+        
+    def track_card(self, x):
+        Dealer.dl.append(x)
+
+    def print_list(self):
+        print Dealer.dl
+
+    def pick_another_card(self):
+        print 'Sum is less than 17. Picking another card for the dealer.'
+        d3 = self.pick_card()
+        self.track_card(d3)
+        print 'Dealer:'
+        self.print_list()
+
+    def sum(self):
+        Dealer.sumd = 0
+        for card in Dealer.dl:
+            if(card == "Ace"):
+                value1 = input('Found an Ace with Dealer. Consider as 1 or 11?')
+                Dealer.sumd += value1
+            elif(card == "Jack" or card == "Queen" or card == "King"):
+                Dealer.sumd += 10
+            else:
+                value2 = int(card)
+                Dealer.sumd += value2
+
+
+    def check_sum(self):
+
+        while Dealer.sumd < 17:
+            self.pick_another_card()
+            self.sum()
+        return Dealer.sumd
